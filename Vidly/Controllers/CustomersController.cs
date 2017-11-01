2accd3e460fed87e144dbf587566ee907dfab8fa
Iliyan <solommr5kk@gmail.com>
2017-11-02 00:09:18 +0200

    Scafold MVC project ++

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Models.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Cust(int? id)
+        {
+            var customer = new Customer()
+            {
+                Id = 1,
+                Name = "John Smith"
+            };
+            var secondCustomer = new Customer()
+            {
+                Id = 2,
+                Name = "Mary Williams"
+            };
+            List<Customer> customers = new List<Customer>();
+            customers.Add(customer);
+            customers.Add(secondCustomer);
+
+            return View(customers);
+        }
+    }
+}
